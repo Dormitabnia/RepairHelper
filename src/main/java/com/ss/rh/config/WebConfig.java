77ecab1a923fd6f49912c98c0cf7a2b98a8d40da
Dormitabnia <wangjingnan557@163.com>
2018-11-18 22:23:17 +0800

    增加自定义注解 增加登录拦截器

--- src/main/java/com/ss/rh/config/WebConfig.java
+++ src/main/java/com/ss/rh/config/WebConfig.java
@@ -1,18 +1,29 @@
 package com.ss.rh.config;
 
+import com.ss.rh.interceptor.AuthenticationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+@Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor()).addPathPatterns("/**");
         super.addInterceptors(registry);
+
     }
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
+    }
+
+    @Bean
+    public AuthenticationInterceptor authenticationInterceptor() {
+        return new AuthenticationInterceptor();
     }
 }
