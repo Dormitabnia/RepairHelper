e0d93956779a2b97b1309bdcdfb4eba0d664d20c
kongmoumou <kongmoumou666@gmail.com>
2019-03-23 13:18:44 +0800

    FE - 增加管理员

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -31,3 +31,11 @@ export function deleteUser(id) {
     data: id,
   });
 }
+
+export function addAdmin({ username, password }) {
+  return request({
+    url: '/admin',
+    method: 'post',
+    data: { username, password },
+  });
+}
